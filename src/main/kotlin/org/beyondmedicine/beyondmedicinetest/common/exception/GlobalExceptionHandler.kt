de76714ddefe71541564275ca65950be117b3908
Kimdongkyun <dgk0911@gmail.com>
2025-03-23 22:08:18 +0900

    [feat/exception] - GlobalExceptionHandler 응답 수정으로 인한 오류 수정

--- src/main/kotlin/org/beyondmedicine/beyondmedicinetest/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/beyondmedicine/beyondmedicinetest/common/exception/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package org.beyondmedicine.beyondmedicinetest.common.exception
 import org.beyondmedicine.beyondmedicinetest.common.dto.ApiResponse
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.validation.BindException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -14,31 +15,31 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessCodeRetryFailException::class, Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    fun handleInternalServerErrorException(e: AccessCodeRetryFailException): ApiResponse<Void> {
+    fun handleInternalServerErrorException(e: AccessCodeRetryFailException): ResponseEntity<ApiResponse<Void>> {
         return ApiResponse.internalServerError(e.message?:"")
     }
 
     @ExceptionHandler(UserAccessCodeNotActivatedException::class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    fun handleForbiddenException(e: UserAccessCodeNotActivatedException): ApiResponse<Void> {
+    fun handleForbiddenException(e: UserAccessCodeNotActivatedException): ResponseEntity<ApiResponse<Void>> {
         return ApiResponse.forbidden(e.message?:"")
     }
 
     @ExceptionHandler(AccessCodeAlreadyActivatedException::class, DataIntegrityViolationException::class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    fun handleConflictException(e: AccessCodeAlreadyActivatedException): ApiResponse<Void> {
+    fun handleConflictException(e: AccessCodeAlreadyActivatedException): ResponseEntity<ApiResponse<Void>> {
         return ApiResponse.conflict(e.message?:"")
     }
 
     @ExceptionHandler(IllegalArgumentException::class, BindException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleBadRequestException(e: IllegalArgumentException): ApiResponse<Void> {
+    fun handleBadRequestException(e: IllegalArgumentException): ResponseEntity<ApiResponse<Void>> {
         return ApiResponse.badRequest(e.message?:"")
     }
 
     @ExceptionHandler(NoSuchElementException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFoundException(e: NoSuchElementException): ApiResponse<Void> {
+    fun handleNotFoundException(e: NoSuchElementException): ResponseEntity<ApiResponse<Void>> {
         return ApiResponse.notFound(e.message?:"")
     }
 
